a1b53172164932c16352e23421f694459e3b9b8b
Jeb129 <elyer129@gmail.com>
2025-12-17 22:31:04 +0300

    Поменял маршруты БД

--- src/utils/api/database.js
+++ src/utils/api/database.js
@@ -1,22 +1,12 @@
-import { publicApi,privateApi } from './apiClient';
+import { publicApi,privateApi } from './axios';
 
 
 export const getObject = async (model_name, filters = {}) => {
-  const response = await publicApi.post('/database/read/', {
-    model: model_name,
-    fields: filters,
-  });
+  const response = await publicApi.post(`/database/get/${model_name}`, filters);
   return response.data;
 };
 
-export const getObjectMeta = async (model_name) => {
-  const response = await publicApi.get(`/database/meta/${model_name}/`);
-  return response.data;
-} 
-export const createObject = async (model_name, fields = {}) => {
-  const response = await privateApi.post('/database/create/', {
-    model: model_name,
-    fields: fields,
-  });
+export const updateObject = async (model_name, data = {}) => {
+  const response = await privateApi.post(`/database/update/${model_name}`, data);
   return response.data;
 };
