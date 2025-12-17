ea243453f1df7d449597703247c9a149f0992507
Jeb129 <elyer129@gmail.com>
2025-12-17 23:52:12 +0300

    Авторизация

--- src/utils/api/auth.js
+++ src/utils/api/auth.js
@@ -1,7 +1,11 @@
 import {privateApi, publicApi} from './axios.js';
 
-export const login = async (username, password) => {
-  const response = await publicApi.post('/auth/access/', { username, password });
+export const register = async (email, password) => {
+  const response = await publicApi.post('/auth/register', { email, password });
+  return response.data;
+};
+export const login = async (email, password) => {
+  const response = await publicApi.post('/auth/access', { email, password });
   localStorage.setItem('access_token', response.data.access);
   localStorage.setItem('refresh_token', response.data.refresh);
   return response.data;
